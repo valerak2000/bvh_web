7754807a22d6654d43c071b8658511ac0aa273f6
valera_k2000 <valera_k2000@inbox.ru>
2018-05-21 14:06:00 +0300

    t6.3

--- backend.sh
+++ backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source py3/bin/activate
-python ./src/manage.py runserver --settings=djangoreactredux.settings.dev_docker
+python ./src/manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev_docker
