c06b24cb66a321223bc2e7f8a4206bbdf7c31069
valera_k2000 <valera_k2000@inbox.ru>
2018-08-06 10:25:00 +0300

    0806

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -3,7 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER djangoreactredux WITH PASSWORD 'password' CREATEDB;
+    CREATE USER djangoreactredux WITH PASSWORD '123' CREATEDB;
     CREATE DATABASE djangoreactredux_dev;
     GRANT ALL PRIVILEGES ON DATABASE djangoreactredux_dev TO djangoreactredux;
 EOSQL
