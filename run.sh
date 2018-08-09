d44b066a0e3f0b5e1f860d4f2c9c878b54835826
valera_k2000 <valera_k2000@inbox.ru>
2018-08-09 16:42:18 +0300

    0809 3

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 cd /var/www/bvh_web
 source py3/bin/activate
 screen -dmS webbg python ./src/manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev_docker
-screen -dmS webfr npm run dev
+screen -dmS webfg npm run dev
 #forever npm run dev >> logfile.log
