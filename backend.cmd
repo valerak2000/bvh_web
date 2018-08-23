0883c64b14bfcb8b9faec101465a04e98c6131fe
valera_k2000 <valera_k2000@inbox.ru>
2018-08-23 16:37:06 +0300

    0823 1

--- backend.cmd
+++ backend.cmd
@@ -1,1 +1,1 @@
-python .\src\manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.prod
+python .\src\manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev
--- backend.sh
+++ backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source py3/bin/activate
-python ./src/manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.prod
+python ./src/manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev
