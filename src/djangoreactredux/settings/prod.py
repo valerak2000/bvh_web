41df0c2c134b8dcec352e92d0e588c6a4a356513
valera_k2000 <valera_k2000@inbox.ru>
2018-08-24 15:32:33 +0300

    0824 1

--- src/djangoreactredux/settings/prod.py
+++ src/djangoreactredux/settings/prod.py
@@ -1,68 +1,5 @@
 from djangoreactredux.settings.base import *  # NOQA (ignore all errors on this line)
 
-
-DEBUG = False
-
-PAGE_CACHE_SECONDS = 1
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoreactredux_dev',
-        'USER': 'djangoreactredux',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': 5433,
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),  # NOQA (ignore all errors on this line)
-    }
-}
-
-REST_FRAMEWORK['EXCEPTION_HANDLER'] = 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['django_rest_logger_handler'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'django_rest_logger_handler': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['django_rest_logger_handler'],
-            'propagate': False,
-        },
-        'django_rest_logger': {
-            'level': 'DEBUG',
-            'handlers': ['django_rest_logger_handler'],
-            'propagate': False,
-        },
-    },
-}
-
-DEFAULT_LOGGER = 'django_rest_logger'
-
-LOGGER_EXCEPTION = DEFAULT_LOGGER
-LOGGER_ERROR = DEFAULT_LOGGER
-LOGGER_WARNING = DEFAULT_LOGGER
-
-""" from djangoreactredux.settings.base import *  # NOQA (ignore all errors on this line)
-
-
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
@@ -143,4 +80,3 @@ DEFAULT_LOGGER = 'raven'
 LOGGER_EXCEPTION = DEFAULT_LOGGER
 LOGGER_ERROR = DEFAULT_LOGGER
 LOGGER_WARNING = DEFAULT_LOGGER
-"""
