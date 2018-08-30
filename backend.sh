a4e692222fe48678902b752fb5eec0ca98505038
valerak2000 <valera_k2000@inbox.ru>
2018-08-30 03:30:49 +0300

    0830

--- backend.sh
+++ backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source py3/bin/activate
 python ./src/manage.py runserver 0.0.0.0:8000 --settings=bvh_web.settings.dev
-gunicorn --bind 0.0.0.0:8000 bvh_web.wsgi
+#gunicorn --bind 0.0.0.0:8000 bvh_web.wsgi
