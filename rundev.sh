f5f19cdb58461bba555e6399eac00a9d224c7f50
valerak2000 <valera_k2000@inbox.ru>
2018-09-13 22:25:03 +0300

    0913 1

--- rundev.sh
+++ rundev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /var/www/bvh_web
+cd /var/www/bvh_web/backend
 source py3/bin/activate
-screen -dmS webbg python ./src/manage.py runserver 0.0.0.0:8080 --settings=bvh_web.settings.dev
+screen -dmS webbg python manage.py runserver 0.0.0.0:8080 --settings=bvh_web.settings.dev
+cd ../frontend
 screen -dmS webfg npm run dev
 #forever npm run dev >> logfile.log
--- runprod.sh
+++ runprod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /var/www/bvh_web
+cd /var/www/bvh_web/backend
 source py3/bin/activate
-cd src
 screen -dmS webbg gunicorn --bind 0.0.0.0:8080 bvh_web.wsgi 
 #--settings=djangoreactredux.settings.prod
+cd ../frontend
 screen -dmS webfg npm run prod
 
