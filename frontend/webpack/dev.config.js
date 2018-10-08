c227fb24a313b7604180b4d033ca1fcf2d6cad42
valera_k2000 <valera_k2000@inbox.ru>
2018-10-08 10:40:06 +0300

    1008 merge from master

--- frontend/webpack/dev.config.js
+++ frontend/webpack/dev.config.js
@@ -9,9 +9,9 @@ const mode = 'development';
 module.exports = merge(commonConfig(mode), {
     mode,
     entry: {
-        'hot': 'react-hot-loader/patch', // activate HMR for React
-        'webpack-dev': 'webpack-dev-server/client?${url}', // bundle the client for webpack-dev-server and connect to the provided endpoint
-        'webpack-hot': 'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
+        hot: 'react-hot-loader/patch', // activate HMR for React
+        webpack_dev: 'webpack-dev-server/client?${url}', // bundle the client for webpack-dev-server and connect to the provided endpoint
+        webpack_hot: 'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
         app: './index.jsx', // the entry point of our app
         vendors: ['react'],
     },
