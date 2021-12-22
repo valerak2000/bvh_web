30277be43badb1de12a0de7c0be4b57b3604af68
valerak2000 <valera_k2000@inbox.ru>
2021-12-23 02:45:14 +0300

    1

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source py37/bin/activate
-python manage.py runserver 0.0.0.0:8080 --settings=bvh_web.settings.dev
+python3 manage.py runserver 0.0.0.0:8080 --settings=bvh_web.settings.dev
 #gunicorn --bind 0.0.0.0:8080 --env DJANGO_SETTINGS_MODULE=bvh_web.settings.prod bvh_web.wsgi
