3e66d15b53993b0972008f6b0b0c90955d87b735
valerak2000 <valera_k2000@inbox.ru>
2026-06-29 01:57:25 +0300

    20260628

--- frontend/src/containers/About/CommonInfo/index.jsx
+++ frontend/src/containers/About/CommonInfo/index.jsx
@@ -14,119 +14,131 @@ function CommonInfoView(props) {
     const { card, ul, li, RedLine } = app;
 
     return (
-        <Card
-            square = { true }
-            sx = { card }
-        >
-            <CardHeader
-                title = 'О компании'
-                { ...props }
-            />
-            <CardContent
-                sx = { card.text }
-            >
+        <Card square={true} sx={card}>
+            <CardHeader title="О компании" {...props} />
+            <CardContent sx={card.text}>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    <strong>ООО «Брюховецкое водопроводное хозяйство» (ООО «БВХ»)</strong> зарегистрировано 23 декабря 2005г.<br />
+                    <strong>ООО «Брюховецкое водопроводное хозяйство» (ООО «БВХ»)</strong>{' '}
+                    зарегистрировано 23 декабря 2005г.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    Основным структурным подразделением ООО «БВХ» является цех водопровода, обеспечивающий круглосуточное обслуживание водозаборов и насосных станций подкачек, ремонт артезианских скважин и водопроводных сетей.<br />
+                    Основным структурным подразделением ООО «БВХ» является цех водопровода,
+                    обеспечивающий круглосуточное обслуживание водозаборов и насосных станций
+                    подкачек, ремонт артезианских скважин и водопроводных сетей.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    Предприятие оказывает услуги на территории Брюховецкого (ст. Брюховецкая, 9 хуторов), Переясловского (ст. Переясловская) и Свободненского (с. Свободное)
-                    сельских поселений муниципального образования Брюховецкий район.<br />
+                    Предприятие оказывает услуги на территории Брюховецкого (ст. Брюховецкая, 9
+                    хуторов), Переясловского (ст. Переясловская) и Свободненского (с. Свободное)
+                    сельских поселений муниципального образования Брюховецкий район.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    Производственную деятельность по холодному водоснабжению ООО «БВХ» начало осуществлять в ст.Брюховецкой с 01.02.2006г, в остальных населенных пунктах с 16.09.2011г.<br />
+                    Производственную деятельность по холодному водоснабжению ООО «БВХ» начало
+                    осуществлять в ст.Брюховецкой с 01.02.2006г, в остальных населенных пунктах с
+                    16.09.2011г.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    В силу геологического расположения добыча воды ведется только из подземных источников артезианских скважин глубиной от 80 до 800 метров.
-                    На обслуживании ООО «БВХ» находится <strong>296,1 км</strong> водопроводных сетей, <strong>39</strong> артезианских скважин и <strong>4</strong> водозабора.<br />
+                    В силу геологического расположения добыча воды ведется только из подземных
+                    источников артезианских скважин глубиной от 80 до 800 метров. На обслуживании
+                    ООО «БВХ» находится <strong>296,1 км</strong> водопроводных сетей,{' '}
+                    <strong>39</strong> артезианских скважин и <strong>4</strong> водозабора.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
                     Фактическая структура абонентов по водопотреблению за 2012 год составляет:
                 </Typography>
-                <ul style = { ul }>
-                    <li style = { li }>
+                <ul style={ul}>
+                    <li style={li}>
                         население <strong>86,8%</strong>
                     </li>
-                    <li style = { li }>
+                    <li style={li}>
                         бюджетные организации <strong>3,1%</strong>
                     </li>
-                    <li style = { li }>
+                    <li style={li}>
                         прочие организации <strong>10.1%</strong>
                     </li>
                 </ul>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    На всех водозаборах перед подачей воды населению производится обеззараживание водопроводных сетей хлорной водой, которая готовится из хлорной извести непосредственно перед внесением.
-                    Хлорирование производится после проведения ремонтных работ и технологических нарушений, по эпидемиологическим показаниям.<br />
+                    На всех водозаборах перед подачей воды населению производится обеззараживание
+                    водопроводных сетей хлорной водой, которая готовится из хлорной извести
+                    непосредственно перед внесением. Хлорирование производится после проведения
+                    ремонтных работ и технологических нарушений, по эпидемиологическим показаниям.
+                    <br />
                 </Typography>
                 <Typography
-                    variant = 'body1'
-                    color = 'textSecondary'
-                    sx = {{
+                    variant="body1"
+                    color="textSecondary"
+                    sx={{
                         margin: 'auto auto auto 0.5rem',
                         textAlign: 'justify',
-                        textIndent: '1.5em',
+                        textIndent: '1.5em'
                     }}
                 >
-                    На период паводков и чрезвычайных ситуаций должен устанавливаться усиленный режим контроля питьевой воды в соответствии с рабочей программой по согласованию с центром «Госсанэпиднадзора».
-                    Производственный контроль качества питьевой воды в соответствии с рабочей программой осуществляется лабораторией ФГУЗ «Центр гигиены и эпидемиологии в Краснодарском крае».<br />
+                    На период паводков и чрезвычайных ситуаций должен устанавливаться усиленный
+                    режим контроля питьевой воды в соответствии с рабочей программой по согласованию
+                    с центром «Госсанэпиднадзора». Производственный контроль качества питьевой воды
+                    в соответствии с рабочей программой осуществляется лабораторией ФГУЗ «Центр
+                    гигиены и эпидемиологии в Краснодарском крае».
+                    <br />
                 </Typography>
             </CardContent>
         </Card>
@@ -134,7 +146,7 @@ function CommonInfoView(props) {
 }
 
 CommonInfoView.propTypes = {
-    theme: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired
 };
 
 export default CommonInfoView;
